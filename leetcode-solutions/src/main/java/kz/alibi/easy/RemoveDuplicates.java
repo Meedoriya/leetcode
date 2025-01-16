067394f73d2c86eb3b0111fc6c70062f02784b20
Alibi <meedoriya@gmail.com>
2025-01-16 14:26:38 +0500

    three easy tasks

---
+++ leetcode-solutions/src/main/java/kz/alibi/easy/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package kz.alibi.easy;
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
