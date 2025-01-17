1946b3f95b67e93f6d2ea92bd805cd3ced42db1b
Alibi <meedoriya@gmail.com>
2025-01-17 20:27:13 +0500

    new three tasks

---
+++ leetcode-solutions/src/main/java/kz/alibi/easy/LengthOfLastWord.java
@@ -0,0 +1,9 @@
+package kz.alibi.easy;
+
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        String[] words = s.trim().split(" ");
+        return words[words.length - 1].length();
+    }
+}
